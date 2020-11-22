7972db320c1cf3079c9f3eb38babff571d7b85d2
Caleb Kang <hoyoul@gmail.com>
2020-11-22 03:27:39 -0700

    feat: capture screencast images and convert to mp4 movie

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const spawn = require('child_process').spawn;
 const sprintf = require('sprintf-js').sprintf;
 
 const CAPTURE_OPTIONS = {
   format: 'jpeg',   // jpeg or png
   quality: 80,      // compression quality 0 ~ 100
+  maxWidth: 1280,
+  maxHeight: 720,
 };
 
 const plugin = {
@@ -20,28 +23,21 @@ const mkdir = (path) => {
   if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
 };
 
-const clientHandler = async (taiko, eventHandler) => {
-  plugin.eventHandler = eventHandler;
-  plugin.eventHandler.on('createdSession', () => {
-    plugin.client = taiko.client();
-  });
-};
-
 const start = async (filePath) => {
-  if (path.extname(filePath) !== 'mp4') throw new Error('Output file should have a .mp4 extension.');
+  if (path.extname(filePath) !== '.mp4') throw new Error('Output file should have a .mp4 extension.');
 
   mkdir(path.dirname(filePath));
 
   plugin.filePath = filePath;
 
-  plugin.client.on('Path.screencastFrame', frame => {
+  plugin.client.on('Page.screencastFrame', frame => {
     plugin.client.send('Page.screencastFrameAck', { sessionId: frame.sessionId });
     plugin.deviceWidth = frame.metadata.deviceWidth;
     plugin.deviceHeight = frame.metadata.deviceHeight;
     plugin.frames.push(frame.data);
   });
 
-  plugin.eventHandler.once('createSession', client => {
+  plugin.eventHandler.once('createdSession', client => {
     plugin.client = client;
     start(filePath);
   });
@@ -62,17 +58,53 @@ const stop = async () => {
   await new Promise(resolve => setTimeout(resolve, 500));
   await pause();
 
-  const baseName = path.basename(plugin.filePath);
+  // Save frame as images first.
+  const filename = path.basename(plugin.filePath);
+  const basename = filename.substring(0, filename.length - 4);
   const directory = path.dirname(plugin.filePath);
-
   for (const [ index, base64Data ] of plugin.frames.entries()) {
-    const imagePath = sprintf('%s/%s%03d.%s',
-      directory, baseName, index + 1, CAPTURE_OPTIONS.format,
-    );
+    const imagePath = sprintf('%s/%s%03d.%s', directory, basename, index + 1, CAPTURE_OPTIONS.format);
     fs.writeFile(imagePath, base64Data, 'base64', error => {
-      console.log(error);
+      if (error) console.error(error);
     });
   }
+
+  // Create a mp4 movie out of the image frames.
+  const cmd = '/usr/local/bin/ffmpeg';
+  const args = [
+    '-y',
+    '-i', sprintf('%s/%s%%03d.%s', directory, basename, CAPTURE_OPTIONS.format),
+    '-s', sprintf('%dx%d', CAPTURE_OPTIONS.maxWidth, CAPTURE_OPTIONS.maxHeight),
+    '-codec:a', 'aac',
+    '-b:a', '44.1k',
+    '-r', '15',
+    '-b:v', '1000k',
+    '-c:v', 'h264',
+    '-f', 'mp4',
+    plugin.filePath,
+  ];
+  const proc = spawn(cmd, args);
+
+  if (process.env.DEBUG) {
+    proc.stdout.on('data', data => console.log(data));
+    proc.stderr.setEncoding('utf8');
+    proc.stderr.on('data', data => console.log(data));
+  }
+
+  // Delete the images upon building a movie successfully.
+  proc.on('close', () => {
+    const regEx = new RegExp(sprintf('%s\\d{3}\\.%s', basename, CAPTURE_OPTIONS.format), 'i');
+    fs.readdirSync(directory)
+      .filter(file => regEx.test(file))
+      .map(file => fs.unlinkSync(sprintf('%s/%s', directory, file)));
+  });
+};
+
+const clientHandler = async (taiko, eventHandler) => {
+  plugin.eventHandler = eventHandler;
+  plugin.eventHandler.on('createdSession', () => {
+    plugin.client = taiko.client();
+  });
 };
 
 module.exports = {
